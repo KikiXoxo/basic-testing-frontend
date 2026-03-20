ffc9e469b1e2a474cf939212c401043a28cdaf37
Shekinah Adeogo <adeogoshekinah@gmail.com>
2026-03-20 16:35:31 +0100

    refactor: move formSubmitHandler parts into modular functions

--- src/util/numbers.js
+++ src/util/numbers.js
@@ -1,3 +1,5 @@
+import { validateNumber, validateStringNotEmpty } from './validation.js';
+
 export function transformToNumber(value) {
   // if (value === undefined) throw new Error('No value provided');
   if (value == null) throw new Error('No value provided');
@@ -5,4 +7,17 @@ export function transformToNumber(value) {
   // 'value === null' checks strictly for null, but 'value == null' checks for both null and undefined
 
   return +value;
+}
+
+export function cleanNumbers(numberValues) {
+  const numbers = [];
+
+  for (const numberInput of numberValues) {
+    validateStringNotEmpty(numberInput);
+    const number = transformToNumber(numberInput);
+    validateNumber(number);
+    numbers.push(number);
+  }
+
+  return numberValues;
 }
