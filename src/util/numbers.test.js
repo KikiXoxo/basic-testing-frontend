90541cdcbf9a7715e4dcaf513ee291b455a56a0f
Shekinah Adeogo <adeogoshekinah@gmail.com>
2026-03-20 15:12:53 +0100

    feat: make numbers.test.js more robust

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -10,10 +10,14 @@ it('should transform a string number to a type number', () => {
 
 it('should yield NaN for non-transformable values', () => {
   const input = 'invalid';
+  const input2 = {};
 
   const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
 
   expect(result).toBeNaN();
+  expect(result2).toBeNaN();
+  // In this test, you can see we have two different inputs and expectations. While usually it is advised for a single test to have a single expectation, in this case, it is fine to have two expectations, because we are testing the same scenario, just with different possible inputs
 });
 
 it('should throw an error if no argument is passed', () => {
